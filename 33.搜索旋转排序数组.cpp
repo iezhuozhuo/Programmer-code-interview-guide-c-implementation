f9d1773789e6a5c5b36419ab7fb6522b6c3437fc
iezhuozhuo <37651166+iezhuozhuo@users.noreply.github.com>
2019-02-15 21:30:22 +0800

    Add files via upload

---
+++ 33.搜索旋转排序数组.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+	    int right = nums.size() - 1;
+	    while (left <= right) {
+		    int mid = left + ((right - left) >> 1);
+		    if (nums[mid] == target) {
+			    return mid;
+		    }
+		    if (nums[mid] < nums[right]) {
+			    if (nums[mid] < target && target <= nums[right]) {
+				    left = mid + 1;
+			    }
+			    else {
+				    right = mid - 1;
+			    }
+		    }
+		    else {
+			    if (nums[left] <= target && target < nums[mid]) {
+				    right = mid - 1;
+			    }
+			    else {
+				    left = mid + 1;
+			    }
+		    }
+	    }
+	    return -1; 
+    }
+};
